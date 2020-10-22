19a2803bfd3859809750bc74e4a6f06ae1f2c493
mholt9 <mholt9@gatech.edu>
2020-10-22 02:02:19 -0400

    first try using AFINN

--- analysis.py
+++ analysis.py
@@ -1,51 +1,20 @@
 import json
 
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.corpus import twitter_samples, stopwords
-from nltk.tag import pos_tag
-from nltk.tokenize import word_tokenize
-from nltk import FreqDist, classify, NaiveBayesClassifier
+# initialize afinn sentiment analyzer
+from afinn import Afinn
 
-import re, string, random
+af = Afinn()
 
-def remove_noise(tweet_tokens, stop_words = ()):
+# data from TripAdvisor
+museum_data_file_path = '/Users/madelineholt/CSclasses/Sentiment-Analysis/Data/review_quotes.json'
+with open(museum_data_file_path, 'r') as json_obj:
+    museum_reviews = json.loads(json_obj.read())
 
-    cleaned_tokens = []
-
-    for token, tag in pos_tag(tweet_tokens):
-        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
-                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+','', token)
-        token = re.sub("(@[A-Za-z0-9_]+)","", token)
-
-        if tag.startswith("NN"):
-            pos = 'n'
-        elif tag.startswith('VB'):
-            pos = 'v'
-        else:
-            pos = 'a'
-
-        lemmatizer = WordNetLemmatizer()
-        token = lemmatizer.lemmatize(token, pos)
-
-        if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
-            cleaned_tokens.append(token.lower())
-    return cleaned_tokens
-
-def get_all_words(cleaned_tokens_list):
-    for tokens in cleaned_tokens_list:
-        for token in tokens:
-            yield token
-
-def get_tweets_for_model(cleaned_tokens_list):
-    for tweet_tokens in cleaned_tokens_list:
-        yield dict([token, True] for token in tweet_tokens)
-
-if __name__ == "__main__":
-
-    #import museum reviews from JSON
-    file_path = '/Users/madelineholt/CSclasses/Sentiment-Analysis/Data/review_quotes.json'
-    with open(file_path, 'r') as json_obj:
-        museum_reviews = json.loads(json_obj.read())
+# compute sentiment scores (polarity) and labels
+sentiment_scores = [af.score(reviews) for reviews in museum_reviews]
+sentiment_category = ['positive' if score > 0
+                      else 'negative' if score < 0
+                        else 'neutral'
+                            for score in sentiment_scores]
 
 
-  
